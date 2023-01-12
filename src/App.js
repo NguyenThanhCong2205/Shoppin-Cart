4d2303e9f6d44d30e140938694aab0255a641c09
Nguyen Thanh Cong <thanhcong5591@gmail.com>
2023-01-11 18:35:07 -0800

    Bai-tap-Shoes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import GrandPa from "./demoProps/GrandPa";
+
+import Home from "./Shopping Cart/Home";
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
